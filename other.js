0133e79ac9a0b1b02f3f7aeb7ecd1cc62df66f4c
NukeWorks <88806815+NukeWorks@users.noreply.github.com>
2021-08-14 21:37:51 -0400

    Update other.js

--- other.js
+++ other.js
@@ -2,7 +2,15 @@
 
 let profileOn = true;
 
+function profileOnDelete() {
+  console.log("Profile Deleted")
+}
+
 if (profileOn == true) 
 {
-  console.log("Nukeworks Profile is real, and is on github.com/NukeWorks")
+  console.log("Nukeworks Profile is real, and is on github.com/NukeWorks");
+}
+else 
+{
+  profileOnDelete();
 }
